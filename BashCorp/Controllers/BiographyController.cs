03bbc3822faec03cf8e2f9517a628585f48d4a35
Cloud Quinn <c.quinn@outlook.com>
2017-06-10 17:45:43 +0100

    Create fade effects on navigation and footer image Start on biography template

---
+++ BashCorp/Controllers/BiographyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BashCorp.Controllers
+{
+    public class BiographyController : Controller
+    {
+        // GET: Biography
+        public ActionResult Cloud()
+        {
+            return View();
+        }
+    }
+}
