b589e915d15fee716e00a096e75f3546a23ffb31
Kaylee Eluvian <kaylee.eluvian@icloud.com>
2017-08-18 12:27:15 +0100

    Migrate to .Net

---
+++ BashCorp.Website/wwwroot/js/site.js
@@ -0,0 +1,86 @@
+﻿const resize = () => {
+                const width = window.innerWidth;
+                const elements = document.getElementsByClassName('pane');
+                Array.from(elements).forEach(i => {
+                    const left = Math.floor((width - i.clientWidth) / 2)
+                    i.style.left = left + "px"
+                });
+                
+                const footer = document.getElementsByClassName('footer')[0];
+                const bottomBanner = document.getElementsByClassName('bottom')[0];
+                const header = document.getElementsByClassName('heading')[0];
+                const glass = document.getElementsByClassName('glass')[0];
+                const view = document.getElementsByClassName('view')[0];
+                const height = document.getElementsByClassName('content')[0].clientHeight;
+                const glassHeight = view.clientHeight - header.clientHeight;
+                glass.style['min-height'] = `${glassHeight}px`
+                glass.style.height = `${height}px`;
+                view.style.height = `${height + header.clientHeight}px`;
+                footer.style.top = bottomBanner.style.top = `${height + header.clientHeight - 50}px`;
+
+            };
+
+            const resizeGallery = ()=> {
+                const slides = document.getElementsByClassName('slide');
+                const carouselFrame = document.getElementById('frame');
+                const topLeft = Math.floor(carouselFrame.clientLeft + (carouselFrame.clientWidth * 0.187));
+                const top = Math.floor(carouselFrame.clientTop + (carouselFrame.clientHeight * 0.154));
+                const screenWidth = Math.floor(carouselFrame.clientWidth * 0.63);
+
+                Array.from(slides).forEach(s => {
+
+                    const screen = s.getElementsByClassName('screen')[0];
+                    screen.style.left = `${topLeft}px`;
+                    screen.style.top = `${top}px`;
+                    screen.style.width = `${screenWidth}px`;
+                    screen.style.display = "block";
+                });
+            }
+
+            const adjustElementHeight = (element, newHeight, initialHeight) => {
+                if (element.clientHeight >= initialHeight)
+                    return;
+
+                element.style.height =  `${element.clientHeight + newHeight}px`;
+                setTimeout(()=> {
+                    adjustElementHeight(element, newHeight, initialHeight);
+                }, 10);
+            };
+
+            const slideDownElement = (element, initialHeight) => {
+                element.style.height = '0px';       
+                const factor = Math.floor(initialHeight / 5);
+                adjustElementHeight(element, factor, initialHeight);
+            };
+
+            const setMenu = ()=> {
+                const triggers = document.getElementsByClassName('submenutrigger');
+                Array.from(triggers).forEach(t => {
+                    const menus = t.parentElement.getElementsByClassName('submenu');
+                    t.onclick = () => {
+                        Array.from(menus).forEach(m => {
+                            m.classList.remove('sr-only');
+                            const initialHeight = m.clientHeight;
+                            m.style.height = '0px';
+                            slideDownElement(m, initialHeight);
+                        });
+
+                        return false;
+                    }
+                });
+            };
+
+            window.onresize = resize;
+            document.addEventListener("DOMContentLoaded", () => {
+                const carouselFrame = document.getElementById('frame');
+                setInterval(resize, 50);
+                const galleryBuilder = () => {
+                    if (carouselFrame.complete)
+                        resizeGallery();
+
+                    setTimeout(galleryBuilder, 50);
+                };
+
+                galleryBuilder();
+                setMenu();
+            })
