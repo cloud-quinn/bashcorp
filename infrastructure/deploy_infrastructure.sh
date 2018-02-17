08dc40232075236c1c3d9352248ae7f9f7b20821
Kaylee Eluvian <kaylee.eluvian@icloud.com>
2018-02-17 12:09:57 +0000

    (no feature) Update infastructure file

--- infrastructure/deploy_infrastructure.sh
+++ infrastructure/deploy_infrastructure.sh
@@ -1,25 +1,9 @@
-echo "Not done yet!!!"
-exit
-sh build_website_image.sh
-sh deploy_website_image.sh
-
 echo "You'll need the private key password."
-az acs kubernetes get-credentials --resource-group BashCorp --name BashCorpCluster
+az aks get-credentials --resource-group BashCorp2 --name BashCorpCluster2
 echo "Deleting old infrastructure..."
 kubectl delete pods,services,replicationcontroller,deployment --all
 echo "Creating new infrastructure..."
 kubectl create -f infrastructure/kubernetes.website.yml
-echo "Waiting for website IP..."
-external_ip=""
-while [ -z $external_ip ]; do
-    sleep 10
-    external_ip=$(kubectl get svc website --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
-done
-cp nginx.conf nginx.release
-sed -i -e 's/WEBSITESERVICE/$external_ip/g' nginx.release
-sh build_reverseproxy_image.sh
-sh deploy_reverseproxy_image.sh
-rm nginx.release
 kubectl create -f infrastructure/kubernetes.reverseproxy.yml
 echo "Waiting for reverse proxy IP..."
 external_ip=""
@@ -27,3 +11,5 @@ while [ -z $external_ip ]; do
     sleep 10
     external_ip=$(kubectl get svc reverseproxy --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
 done
+sh /vagrant/deploy.sh
+echo "Todo: log in to Azure and repoint domain to $external_ip"
